38062998b1f0cd7948d452bea85eb4df423dc41d
Vince Nguyen <vnguye3@uw.edu>
2021-08-02 09:38:19 -0700

    Instantiating Object Classes

--- TriviaMazeGame/src/Player.java
+++ TriviaMazeGame/src/Player.java
@@ -1,6 +1,50 @@
 public class Player {
 
-    public Player(int initalPositionX, int initalPositionY) {
+    Integer myXPosition;
+    Integer myYPosition;
+    Integer myAvailableHints;
 
+    public Player (Integer theInitialX, Integer theInitialY) {
+        setPlayerPosition(theInitialX, theInitialY);
+        myAvailableHints = 3;
+    }
+
+    public void movePlayerNorth() {
+        setPlayerPosition(myXPosition, myYPosition - 1);
+    }
+
+    public void movePlayerSouth() {
+        setPlayerPosition(myXPosition, myYPosition + 1);
+    }
+
+    public void movePlayerEast() {
+        setPlayerPosition(myXPosition + 1, myYPosition);
+    }
+
+    public void movePlayerWest() {
+        setPlayerPosition(myXPosition - 1, myYPosition);
+    }
+
+    public void setPlayerPosition(Integer theXPosition, Integer theYPosition) {
+        myXPosition = theXPosition;
+        myYPosition = theYPosition;
+    }
+
+    public Integer getPlayerXPosition() {
+        return myXPosition;
+    }
+
+   public Integer getPlayerYPosition() {
+       return myYPosition;
+   }
+
+    public void useHint() {
+        if (myAvailableHints >0) {
+            myAvailableHints = myAvailableHints - 1;
+        }
+    }
+
+    public Integer getAvailableHints() {
+        return myAvailableHints;
     }
 }
